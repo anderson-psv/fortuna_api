92099024bb191d82de5b8c586e06d6d7390d7590
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-06-09 22:53:44 -0300

    -Alterado Controller para classe.

--- public/index.php
+++ public/index.php
@@ -3,9 +3,8 @@
 use Fortuna\Controller\UsuarioController;
 
 require_once '../vendor/autoload.php';
-require_once __DIR__ . '/../app/bootstrap/bootstrap.php';
 
 $app = Slim\Factory\AppFactory::create();
-$app->setBasePath("/fortuna_api/index.php");
-$app = UsuarioController::getRoutes($app);
+$app->setBasePath('/fortuna');
+UsuarioController::getRoutes($app);
 $app->run();
