bbe9c6cd102342969ee39361c77744640f76745a
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-06-10 21:13:42 -0300

    -Movido index para pasta root

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 
 
-require_once '../vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 $app = Slim\Factory\AppFactory::create();
 
-$files = opendir(dirname(__DIR__) . '/app/Routes/');
+$files = opendir('app/Routes/');
 
 while ($file = readdir($files)) {
     if(pathinfo($file, PATHINFO_EXTENSION) == 'php') {
-        require_once dirname(__DIR__) . "/app/Routes/$file";
+        require_once "app/Routes/$file";
     }
 }
 
