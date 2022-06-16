743127e65e69863382139aef764b7feb1bf4b7b1
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-06-15 21:59:29 -0300

    -Add listagem de produtos inicial.

--- app/Routes/Admin.php
+++ app/Routes/Admin.php
@@ -18,3 +18,29 @@ $app->get('/admin', function (Request $request, Response $response, $args) use (
 
     return $response->withStatus(200);
 });
+
+$app->get('/admin/produtos', function (Request $request, Response $response, $args) use ($resource_path) {
+    #UsuarioAdmin::checkLogin();
+
+    $page = new Page([
+        'header' => false,
+        'data' => [
+            'site_titulo' => 'Produtos',
+            'res_path'    => $resource_path
+        ]
+    ], '/views/admin/');
+
+    $page->setTpl("admin_produtos", [
+        'produtos' => [
+            0 => [
+                'id'       => 1,
+                'nome'     => 'Produto 1',
+                'descricao' => 'Descrição do produto 1',
+                'valor'    => 10,
+                'status'   => 'ATIVO'
+            ]
+        ]
+    ]);
+
+    return $response->withStatus(200);
+});
