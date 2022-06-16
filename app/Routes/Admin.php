e63ca7a07bb2ad262b369c722fefec10df594a6c
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-06-15 22:45:48 -0300

    -Add formulario para cadastrar produto. -Add rota para cadastro de produto.

--- app/Routes/Admin.php
+++ app/Routes/Admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use Fortuna\Page;
+use Lazer\Classes\Database as Lazer;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -34,13 +35,53 @@ $app->get('/admin/produtos', function (Request $request, Response $response, $ar
         'produtos' => [
             0 => [
                 'id'       => 1,
-                'nome'     => 'Produto 1',
                 'descricao' => 'Descrição do produto 1',
                 'valor'    => 10,
                 'status'   => 'ATIVO'
             ]
         ]
     ]);
+
+    return $response->withStatus(200);
+});
+
+$app->get('/admin/produto/cadastro', function (Request $request, Response $response, $args) use ($resource_path) {
+    #UsuarioAdmin::checkLogin();
+
+    $page = new Page([
+        'header' => false,
+        'data' => [
+            'site_titulo' => 'Cadastrar Produto',
+            'res_path'    => $resource_path
+        ]
+    ], '/views/admin/');
+
+    $page->setTpl("admin_produto_cadastro", []);
+
+    return $response->withStatus(200);
+});
+
+$app->post('/admin/produto/cadastro', function (Request $request, Response $response, $args) use ($resource_path) {
+    #UsuarioAdmin::checkLogin();
+
+    try {
+        $produto = new Produto();
+
+        if (!$produto->save()) {
+            throw new \Exception('Erro ao salvar produto', 7400);
+        }
+
+        $response->getBody()->write(json_encode([
+            'status'  => 'success',
+            'message' => 'Produto cadastrado com sucesso!'
+        ]));
+    } catch (\Throwable $th) {
+        $response->getBody()->write(json_encode([
+            'status'  => 'error',
+            'message' => $th->getMessage()
+        ]));
+    }
+
 
     return $response->withStatus(200);
 });
